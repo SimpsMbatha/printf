7d06a904f1a0c7885a76b8c64ad67ccf1814d0c1
SimpsMbatha <simbatha41@gmail.com>
2023-08-22 07:48:56 -0700

    update

--- _printf.c
+++ _printf.c
@@ -23,23 +23,29 @@ void print_char(char c)
 
 void print_string(const char *s)
 {
-	if (s !=NULL)
+	if (s)
 	{
 		printf("%s", s);
+	}
+	else
+	{
+		return;
 	}
 }
 
 int _printf(const char *format, ...)
 {
+	int i = 0;
+	int c;
+	char *s;
+	va_list args;
+
+	va_start(args, format);
+
 	if (format == NULL)
 	{
-		return -1;
+		return (-1);
 	}
-
-	int i = 0;
-	int count = 0;
-	va_list args;
-	va start(args, format);
 
 	while (format[i] != '\0')
 	{
@@ -50,41 +56,42 @@ int _printf(const char *format, ...)
 			if (format[i] == '\0')
 			{
 				va_end(args);
-				return -1;
+				return (-1);
+			}
+
+			else if (format[i] == ' ')
+			{
+				va_end(args);
+				return (-1);
 			}
 
 			else if (format[i] == 'c')
 			{
-			        int c = va_arg(args, int);
+				c = va_arg(args, int);
 				print_char(c);
-				count++;
 			}
 			else if (format[i] == 's')
 			{
-				char *s = va_arg(args, char *);
+				s = va_arg(args, char *);
 				print_string(s);
-				count += (s != NULL) ? strlen(s) : 0;
 			}
 			else if (format[i] == '%')
 			{
 				putchar('%');
-				count++;
 			}
 			else
 			{
 				putchar('%');
 				putchar(format[i]);
-				count += 2;
 			}
 		}
 		else
 		{
 			print_char(format[i]);
-			count++;
 		}
 		i++;
 	}
 	va_end(args);
 
-	return count;
+	return (i);
 }
